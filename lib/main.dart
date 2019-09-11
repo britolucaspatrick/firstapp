4c0df35eec8f97eac975d6d114731f16ddba60f1
patri <patricklucas524@gmail.com>
2019-09-11 08:58:17 -0300

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -7,31 +7,20 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Iniciando Flutter',
+      title: 'Iniciando calculadora',
       theme: ThemeData(
         primarySwatch: Colors.red
       ),
-      home: MyHomePage(title: 'Flutter'),
+      home: MyHomePage(title: 'calculadora'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -39,122 +28,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      drawer: new Drawer(
-        child: new ListView(
-          children: <Widget>[
-            //aqui vai o avatar com informações 
-            new UserAccountsDrawerHeader(
-              accountEmail: new Text("marcelo.braga@gmail.com"),
-              accountName: new Text("Marcelo Preti Braga"),
-              currentAccountPicture: new GestureDetector(
-                onTap: () => new Tooltip(
-                  message: "Texto",
-                  child: new Text("Exemplo"),
-                ),
-                child: new CircleAvatar(
-                  backgroundImage: new NetworkImage("https://observatoriodocinema.bol.uol.com.br/wp-content/uploads/2019/08/Thanos.png"),
-                ),
-              ),
-            ), 
-
-            new ListTile(
-              title: new Text("Exemplo de Menu Lateral"),
-              trailing: new Icon(Icons.add_alert),
-            ), 
-            new ListTile(
-              title: new Text("Formulário"),
-              trailing: new Icon(Icons.account_circle),
-              onTap: ()  {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => 
-                Tela()));
-              },
-            ), 
-            new ListTile(
-              title: new Text("Cálculo"),
-              trailing: new Icon(Icons.add_box),
-              onLongPress: ()  {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => 
-                Calculo()));
-              },
-            ), 
-            new ListTile(
-              title: new Text("GET Contact"),
-              trailing: new Icon(Icons.add_box),
-              onTap: ()  {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => 
-                ContatoGet()));
-              },
-            )
-          ],
-        ),    
-      ),//
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        /*child: Card(
-          child: InkWell(
-            splashColor: Colors.blue.withAlpha(30),
-            onTap: () {
-              print('Card tapped.');
-            },
-            child: Container(
-              width: 300,
-              height: 100,
-              child: Text('A card that can be tapped'),
-            ),
+        child: new TextField(
+          obscureText: true,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: 'primeiro numero',
           ),
         ),
-        */
-        child: new CupertinoButton(
-          child: new Text("Teste"),
-          color: Colors.red,
-        ),
-
-/*        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Pressione o Botão:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),*/
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      )
     );
   }
 }
